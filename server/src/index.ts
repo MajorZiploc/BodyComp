affd6e5349e17a0f881d0387dc20352012232eec
Manyu <LakeMonYew@gmail.com>
2020-09-09 10:47:40 -0500

    Adds server index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,29 @@
 import { Server, Request, ResponseToolkit } from "@hapi/hapi";
 const init = async () => {
-    const server: Server = new Server({
-        port: 3000,
-        host: 'localhost'
-    });
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request: Request, h: ResponseToolkit) => {
-            return 'Hello World!';
-        }
-    });
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  const server: Server = new Server({
+    port: 3000,
+    host: "localhost",
+  });
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request: Request, h: ResponseToolkit) => {
+      return "Hello World!";
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/foo",
+    handler: (request: Request, h: ResponseToolkit) => {
+      return { foo: "bar" };
+    },
+  });
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 init();
