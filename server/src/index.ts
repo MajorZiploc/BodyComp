63d2e7bbad6892570f83551fad3a0f6db050a72f
Manyu <LakeMonYew@gmail.com>
2020-09-11 13:41:27 -0500

    Adds working db query attempt

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,31 @@
 import { Server, Request, ResponseToolkit } from "@hapi/hapi";
+
+import * as ADODB from "node-adodb";
+
+const connection = ADODB.open(
+  "Provider=MSOLEDBSQL;Server=(localdb)\\MSSQLLocalDB;Database=BodyComp;Trusted_Connection=yes;"
+);
+
+let command = "select * from dbo.Day;";
+
+connection
+  .query(command)
+  .then((data) => {
+    console.log("here");
+    console.log(data);
+    console.log(JSON.stringify(data, null, 2));
+  })
+  .catch((error) => {
+    console.log("here2");
+    console.error(error);
+  });
+
 const init = async () => {
   const server: Server = new Server({
     port: 3000,
     host: "localhost",
   });
+
   server.route({
     method: "GET",
     path: "/",
@@ -22,8 +44,10 @@ const init = async () => {
   await server.start();
   console.log("Server running on %s", server.info.uri);
 };
+
 process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
+
 init();
