05c0f6465f36e2af1afedd9e1bf0a83816440d10
Manyu <LakeMonYew@gmail.com>
2020-09-23 11:13:25 -0500

    WIP form

--- client/src/Components/CSVForm.tsx
+++ client/src/Components/CSVForm.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Button, FormGroup, FormFile } from 'react-bootstrap';
 
 function CSVForm() {
+  function onSubmit(e: any) {
+    var x = e.target.files[0];
+    console.log('submitted');
+    console.log(e);
+  }
+
+  function onSelect(e: any) {
+    console.log('submitted');
+    console.log(e);
+  }
+
   return (
-    <Form>
-      <Form.Group>
-        <Form.File id='CSVFileUpload' label='Upload Daily metrics CSV' multiple type='file' />
-      </Form.Group>
+    <Form onSubmit={(e: any) => onSubmit(e)}>
+      <FormGroup role='form'>
+        <FormFile
+          id='CSVFileUpload'
+          label='Upload Daily metrics CSV'
+          multiple
+          type='file'
+          onSelect={(e: any) => onSelect(e)}
+        />
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+      </FormGroup>
     </Form>
   );
 }
