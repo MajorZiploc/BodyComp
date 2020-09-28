3f63c5b571bbbe95f5b198008f40c2c3c068a5c6
Manyu <LakeMonYew@gmail.com>
2020-09-28 09:11:16 -0500

    Adds bf and mm loss to summary

--- client/src/Components/Summary.tsx
+++ client/src/Components/Summary.tsx
@@ -8,9 +8,11 @@ import { withRouter } from 'react-router-dom';
 import { Carousel } from 'react-bootstrap';
 import '../Styles/Carousel.css';
 
-interface Summary {
+interface SummaryState {
   weightGainOrLoss?: number;
   weightUnits?: string;
+  muscleGainOrLoss?: number;
+  bodyFatGainOrLoss?: number;
   // avgWeight: number;
   // avgCalories: number;
 }
@@ -19,13 +21,61 @@ export interface SummaryProps {
   days?: Day[];
 }
 
+function notNullOrUndefined(thing: any) {
+  return thing !== null && thing !== undefined;
+}
+
+function gainOrLoss(l: number) {
+  return (l > 0 ? 'Gain' : 'Loss') + ': ';
+}
+
+function formartGainOrLossNumber(n: number) {
+  return Math.abs(n).toFixed(2);
+}
+
 function getWeightGainOrLoss(days?: Day[]) {
   if (days) {
-    const daysWithMorningWeight = days.filter(d => d.DyMorningWeight !== null && d.DyMorningWeight !== undefined);
+    const daysWithMorningWeight = days.filter(d => notNullOrUndefined(d.DyMorningWeight));
     const lastDayWeight = daysWithMorningWeight[daysWithMorningWeight?.length - 1]?.DyMorningWeight;
     const firstDayWeight = daysWithMorningWeight[0]?.DyMorningWeight;
     if (firstDayWeight && lastDayWeight) {
       return lastDayWeight - firstDayWeight;
+    }
+  }
+  return undefined;
+}
+
+function getMuscleMassGainOrLoss(days?: Day[]) {
+  if (days) {
+    const dWithWMM = days.filter(d => [d.DyMorningWeight, d.DyMuscleMassPercentage].every(notNullOrUndefined));
+    const lastDay = dWithWMM[dWithWMM?.length - 1];
+    const firstDay = dWithWMM[0];
+    const lastDayWeight = lastDay?.DyMorningWeight;
+    const firstDayWeight = firstDay?.DyMorningWeight;
+    const lastDayMM = lastDay?.DyMuscleMassPercentage;
+    const firstDayMM = firstDay?.DyMuscleMassPercentage;
+    if (firstDayWeight && lastDayWeight && lastDayMM && firstDayMM) {
+      const firstDayMMInWeight = firstDayWeight * firstDayMM * 0.01;
+      const lastDayMMInWeight = lastDayWeight * lastDayMM * 0.01;
+      return lastDayMMInWeight - firstDayMMInWeight;
+    }
+  }
+  return undefined;
+}
+
+function getBodyFatGainOrLoss(days?: Day[]) {
+  if (days) {
+    const dWithWBF = days.filter(d => [d.DyMorningWeight, d.DyBodyFatPercentage].every(notNullOrUndefined));
+    const lastDay = dWithWBF[dWithWBF?.length - 1];
+    const firstDay = dWithWBF[0];
+    const lastDayWeight = lastDay?.DyMorningWeight;
+    const firstDayWeight = firstDay?.DyMorningWeight;
+    const lastDayBF = lastDay?.DyBodyFatPercentage;
+    const firstDayBF = firstDay?.DyBodyFatPercentage;
+    if (firstDayWeight && lastDayWeight && lastDayBF && firstDayBF) {
+      const firstDayBFInWeight = firstDayWeight * firstDayBF * 0.01;
+      const lastDayBFInWeight = lastDayWeight * lastDayBF * 0.01;
+      return lastDayBFInWeight - firstDayBFInWeight;
     }
   }
   return undefined;
@@ -37,17 +87,37 @@ function getWeightUnits(days?: Day[]) {
 }
 
 export default function Summary(summaryProps: SummaryProps) {
-  const summary = {
+  const summary: SummaryState = {
     weightGainOrLoss: getWeightGainOrLoss(summaryProps.days),
     weightUnits: getWeightUnits(summaryProps.days),
+    bodyFatGainOrLoss: getBodyFatGainOrLoss(summaryProps.days),
+    muscleGainOrLoss: getMuscleMassGainOrLoss(summaryProps.days),
   };
 
   return (
     <>
-      {summary && summary.weightGainOrLoss && (
+      {summary && (
         <p>
-          {summary.weightGainOrLoss > 0 ? 'Weight Gain: ' : 'Weight Loss: '}
-          {Math.abs(summary.weightGainOrLoss).toFixed(2) + ' ' + summary.weightUnits}
+          {summary.weightGainOrLoss && (
+            <>
+              {'Weight ' + gainOrLoss(summary.weightGainOrLoss)}
+              {formartGainOrLossNumber(summary.weightGainOrLoss) + ' ' + summary.weightUnits}
+            </>
+          )}
+          <br />
+          {summary.bodyFatGainOrLoss && (
+            <>
+              {'Body Fat ' + gainOrLoss(summary.bodyFatGainOrLoss)}
+              {formartGainOrLossNumber(summary.bodyFatGainOrLoss) + ' ' + summary.weightUnits}
+            </>
+          )}
+          <br />
+          {summary.muscleGainOrLoss && (
+            <>
+              {'Muscle Mass ' + gainOrLoss(summary.muscleGainOrLoss)}
+              {formartGainOrLossNumber(summary.muscleGainOrLoss) + ' ' + summary.weightUnits}
+            </>
+          )}
         </p>
       )}
     </>
