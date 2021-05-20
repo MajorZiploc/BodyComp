dadb25b8336fcab074f63506d855e9235aca30ca
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-20 16:26:00 -0500

    Adds body comp models

--- server/body_comp/models.py
+++ server/body_comp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class WeightUnits(models.Model):
+    name = models.CharField(max_length=250)
+    label = models.CharField(max_length=100)
+
+    def __str__(self):
+            return self.name
+
+class Day(models.Model):
+    weight_units = models.ForeignKey(WeightUnits, on_delete=models.CASCADE)
+    calories = models.IntegerField(default=0)
+    morning_weight = models.FloatField(default=0.0)
+    body_fat_percentage = models.FloatField(default=0.0)
+    muscle_mass_percentage = models.FloatField(default=0.0)
+    day_date = models.DateField()
+
+    def __str__(self):
+            return f'{self.day_date} {self.calories}'
+
