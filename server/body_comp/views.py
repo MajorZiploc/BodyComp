dd9af6f6b3d8a945d5377da10356a858a5c9891e
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-21 11:14:30 -0500

    Adds basic views for day

--- server/body_comp/views.py
+++ server/body_comp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Day, WeightUnit
+from django.utils import timezone
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'body_comp/index.html'
+    context_object_name = 'day_list'
+
+    def get_queryset(self):
+        """
+        """
+        return Day.objects.filter(
+            day_date__lte=timezone.now()
+        ).order_by('-day_date')[:]
+
+class DetailView(generic.DetailView):
+    model = Day
+    template_name = 'body_comp/detail.html'
