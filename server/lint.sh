c2215a10ab475cda07d803385e79acf65b1a566f
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-21 10:09:08 -0500

    Adds linting script

---
+++ server/lint.sh
@@ -0,0 +1,6 @@
+echo "Linting project..."
+
+find . -regextype egrep -iregex ".*\.py" -type f -not -path '*/__pycache__/*' -not -path '*/bin/*' -not -path '*/obj/*' -not -path '*/.git/*' -not -path '*/.svn/*' -not -path '*/node_modules/*' -not -path '*/.ionide/*' -exec autopep8 --in-place --aggressive --aggressive "{}" \; 
+
+echo "Projected Linted!"
+
