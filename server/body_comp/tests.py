4c776c884f9933f2b9772480ab4709155fa6f618
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-22 11:11:51 -0500

    Adds first index view test

--- server/body_comp/tests.py
+++ server/body_comp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class DayIndexViewTests(TestCase):
+  def test_no_days(self):
+    """
+    If no days exist, an appropriate message is displayed.
+    """
+    response = self.client.get(reverse('body_comp:index'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "No days are available.")
+    self.assertQuerysetEqual(response.context['day_list'], [])
