b3ca6717fec5ddb3710828b79214e6c7ba308aec
Manyu Lakhotia <lakemonyew@gmail.com>
2021-05-23 09:13:50 -0500

    Rns home -> day_create

--- server/body_comp/urls.py
+++ server/body_comp/urls.py
@@ -6,5 +6,5 @@ app_name = 'body_comp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('home', views.home_view, name='home')
+    path('day_create', views.day_create_view, name='day_create')
 ]
--- server/body_comp/views.py
+++ server/body_comp/views.py
@@ -5,13 +5,13 @@ from django.utils import timezone
 from .forms import DayForm
 
 
-def home_view(request):
+def day_create_view(request):
   context = {}
   form = DayForm(request.POST or None, request.FILES or None)
   if form.is_valid():
     form.save()
   context['form'] = form
-  return render(request, "body_comp/home.html", context)
+  return render(request, "body_comp/day_create.html", context)
 
 
 class IndexView(generic.ListView):
